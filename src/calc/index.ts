8c811b2b3c5b50f5311f60b6009d34353f89e4af
Thiago Honorato <30918302+hokauz@users.noreply.github.com>
2025-06-12 08:13:40 -0300

    init

---
+++ src/calc/index.ts
@@ -0,0 +1,2 @@
+// cries as funções basicas de uma calculadora
+// sumar, dividir, multiplicar, subtrair
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// crie um programa que mostre as opções de operações de uma calculadora para o usuário selecionar via terminal e depois peça para o usuário os números para realizar a operação
+// depois mostre o resultado da operação
+// prompt
+function main() {}
+
+main();
